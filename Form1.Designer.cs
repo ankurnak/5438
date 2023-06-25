6abbbb1436c2ca3ef2a74aff0a45796e154bbc13
ankurnak <112162607+ankurnak@users.noreply.github.com>
2023-06-25 23:39:31 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace vector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addition = new System.Windows.Forms.Button();
+            this.substraction = new System.Windows.Forms.Button();
+            this.dotproduct = new System.Windows.Forms.Button();
+            this.length = new System.Windows.Forms.Button();
+            this.angle = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.vectors = new System.Windows.Forms.Label();
+            this.txtX1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtY1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtZ1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtX2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtY2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtZ2 = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // addition
+            // 
+            this.addition.Location = new System.Drawing.Point(52, 326);
+            this.addition.Name = "addition";
+            this.addition.Size = new System.Drawing.Size(75, 23);
+            this.addition.TabIndex = 0;
+            this.addition.Text = "addition";
+            this.addition.UseVisualStyleBackColor = true;
+            this.addition.Click += new System.EventHandler(this.addition_Click);
+            // 
+            // substraction
+            // 
+            this.substraction.Location = new System.Drawing.Point(154, 326);
+            this.substraction.Name = "substraction";
+            this.substraction.Size = new System.Drawing.Size(75, 23);
+            this.substraction.TabIndex = 1;
+            this.substraction.Text = "substraction";
+            this.substraction.UseVisualStyleBackColor = true;
+            this.substraction.Click += new System.EventHandler(this.substraction_Click);
+            // 
+            // dotproduct
+            // 
+            this.dotproduct.Location = new System.Drawing.Point(263, 326);
+            this.dotproduct.Name = "dotproduct";
+            this.dotproduct.Size = new System.Drawing.Size(75, 23);
+            this.dotproduct.TabIndex = 2;
+            this.dotproduct.Text = "dotproduct";
+            this.dotproduct.UseVisualStyleBackColor = true;
+            this.dotproduct.Click += new System.EventHandler(this.dotproduct_Click);
+            // 
+            // length
+            // 
+            this.length.Location = new System.Drawing.Point(386, 326);
+            this.length.Name = "length";
+            this.length.Size = new System.Drawing.Size(75, 23);
+            this.length.TabIndex = 3;
+            this.length.Text = "length";
+            this.length.UseVisualStyleBackColor = true;
+            this.length.Click += new System.EventHandler(this.length_Click);
+            // 
+            // angle
+            // 
+            this.angle.Location = new System.Drawing.Point(500, 326);
+            this.angle.Name = "angle";
+            this.angle.Size = new System.Drawing.Size(75, 23);
+            this.angle.TabIndex = 4;
+            this.angle.Text = "angle";
+            this.angle.UseVisualStyleBackColor = true;
+            this.angle.Click += new System.EventHandler(this.angle_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(263, 48);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(156, 88);
+            this.pictureBox.TabIndex = 5;
+            this.pictureBox.TabStop = false;
+            // 
+            // vectors
+            // 
+            this.vectors.AutoSize = true;
+            this.vectors.Location = new System.Drawing.Point(323, 154);
+            this.vectors.Name = "vectors";
+            this.vectors.Size = new System.Drawing.Size(51, 16);
+            this.vectors.TabIndex = 6;
+            this.vectors.Text = "vectors";
+            // 
+            // txtX1
+            // 
+            this.txtX1.Location = new System.Drawing.Point(129, 184);
+            this.txtX1.Name = "txtX1";
+            this.txtX1.Size = new System.Drawing.Size(100, 22);
+            this.txtX1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "x1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 226);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "y1";
+            // 
+            // txtY1
+            // 
+            this.txtY1.Location = new System.Drawing.Point(129, 226);
+            this.txtY1.Name = "txtY1";
+            this.txtY1.Size = new System.Drawing.Size(100, 22);
+            this.txtY1.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 268);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "z1";
+            // 
+            // txtZ1
+            // 
+            this.txtZ1.Location = new System.Drawing.Point(129, 265);
+            this.txtZ1.Name = "txtZ1";
+            this.txtZ1.Size = new System.Drawing.Size(100, 22);
+            this.txtZ1.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(312, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "x2";
+            // 
+            // txtX2
+            // 
+            this.txtX2.Location = new System.Drawing.Point(371, 187);
+            this.txtX2.Name = "txtX2";
+            this.txtX2.Size = new System.Drawing.Size(100, 22);
+            this.txtX2.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(312, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 16);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "y2";
+            // 
+            // txtY2
+            // 
+            this.txtY2.Location = new System.Drawing.Point(371, 223);
+            this.txtY2.Name = "txtY2";
+            this.txtY2.Size = new System.Drawing.Size(100, 22);
+            this.txtY2.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(311, 268);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 16);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "z2";
+            // 
+            // txtZ2
+            // 
+            this.txtZ2.Location = new System.Drawing.Point(371, 262);
+            this.txtZ2.Name = "txtZ2";
+            this.txtZ2.Size = new System.Drawing.Size(100, 22);
+            this.txtZ2.TabIndex = 18;
+            // 
+            // lblResult
+            // 
+            this.lblResult.Location = new System.Drawing.Point(617, 220);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(100, 22);
+            this.lblResult.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(547, 223);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 16);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtZ2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtY2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtX2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtZ1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtY1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtX1);
+            this.Controls.Add(this.vectors);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.angle);
+            this.Controls.Add(this.length);
+            this.Controls.Add(this.dotproduct);
+            this.Controls.Add(this.substraction);
+            this.Controls.Add(this.addition);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addition;
+        private System.Windows.Forms.Button substraction;
+        private System.Windows.Forms.Button dotproduct;
+        private System.Windows.Forms.Button length;
+        private System.Windows.Forms.Button angle;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label vectors;
+        private System.Windows.Forms.TextBox txtX1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtY1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtZ1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtX2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtY2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtZ2;
+        private System.Windows.Forms.TextBox lblResult;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vector
+{
+    public partial class Form1 : Form
+    {
+        private Vector3D vector1;
+        private Vector3D vector2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox.Paint += new PaintEventHandler(pictureBox_Paint);
+        }
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Black, 2);
+
+            int width = pictureBox.Width;
+            int height = pictureBox.Height;
+
+            Point origin = new Point(width / 2, height / 2);
+            int vector1Length = (int)(vector1.Length() * 10);
+            int vector2Length = (int)(vector2.Length() * 10);
+
+            Point vector1End = new Point(origin.X + (int)(vector1.X * vector1Length), origin.Y + (int)(vector1.Y * vector1Length));
+            Point vector2End = new Point(origin.X + (int)(vector2.X * vector2Length), origin.Y + (int)(vector2.Y * vector2Length));
+
+            g.DrawLine(pen, origin, vector1End);
+            g.DrawLine(pen, origin, vector2End);
+        }
+        private void SetButtonStyle(Button button)
+        {
+            button.BackColor = Color.Bisque;
+            button.ForeColor = Color.White;
+            button.Font = new Font("Arial", 12, FontStyle.Bold);
+            button.FlatStyle = FlatStyle.Flat;
+            button.FlatAppearance.BorderSize = 0;
+        }
+
+        private void SetTextBoxStyle(TextBox textBox)
+        {
+            textBox.BackColor = Color.FromArgb(240, 240, 240);
+            textBox.Font = new Font("Arial", 10, FontStyle.Regular);
+        }
+
+        private void SetLabelStyle(Label label)
+        {
+            label.Font = new Font("Arial", 10, FontStyle.Regular);
+        }
+
+        private void addition_Click(object sender, EventArgs e)
+        {
+            double x1 = double.Parse(txtX1.Text);
+            double y1 = double.Parse(txtY1.Text);
+            double z1 = double.Parse(txtZ1.Text);
+            vector1 = new Vector3D(x1, y1, z1);
+
+            double x2 = double.Parse(txtX2.Text);
+            double y2 = double.Parse(txtY2.Text);
+            double z2 = double.Parse(txtZ2.Text);
+            vector2 = new Vector3D(x2, y2, z2);
+
+            Vector3D sum = vector1 + vector2;
+            lblResult.Text = "Sum: " + sum.ToString();
+
+         
+            pictureBox.Refresh();
+        }
+
+        private void substraction_Click(object sender, EventArgs e)
+        {
+            double x1 = double.Parse(txtX1.Text);
+            double y1 = double.Parse(txtY1.Text);
+            double z1 = double.Parse(txtZ1.Text);
+            vector1 = new Vector3D(x1, y1, z1);
+
+            double x2 = double.Parse(txtX2.Text);
+            double y2 = double.Parse(txtY2.Text);
+            double z2 = double.Parse(txtZ2.Text);
+            vector2 = new Vector3D(x2, y2, z2);
+
+            Vector3D difference = vector1 - vector2;
+            lblResult.Text = "Difference: " + difference.ToString();
+
+            // Перемалювання PictureBox
+            pictureBox.Refresh();
+        }
+
+        private void dotproduct_Click(object sender, EventArgs e)
+        {
+            double x1 = double.Parse(txtX1.Text);
+            double y1 = double.Parse(txtY1.Text);
+            double z1 = double.Parse(txtZ1.Text);
+            vector1 = new Vector3D(x1, y1, z1);
+
+            double x2 = double.Parse(txtX2.Text);
+            double y2 = double.Parse(txtY2.Text);
+            double z2 = double.Parse(txtZ2.Text);
+            vector2 = new Vector3D(x2, y2, z2);
+
+            double dotProduct = vector1 * vector2;
+            lblResult.Text = "Dot Product: " + dotProduct.ToString();
+
+            // Перемалювання PictureBox
+            pictureBox.Refresh();
+        }
+
+        private void length_Click(object sender, EventArgs e)
+        {
+            double x1 = double.Parse(txtX1.Text);
+            double y1 = double.Parse(txtY1.Text);
+            double z1 = double.Parse(txtZ1.Text);
+            vector1 = new Vector3D(x1, y1, z1);
+
+            double length = vector1.Length();
+            lblResult.Text = "Length of Vector: " + length.ToString();
+
+          
+            pictureBox.Refresh();
+        }
+
+        private void angle_Click(object sender, EventArgs e)
+        {
+            double x1 = double.Parse(txtX1.Text);
+            double y1 = double.Parse(txtY1.Text);
+            double z1 = double.Parse(txtZ1.Text);
+            vector1 = new Vector3D(x1, y1, z1);
+
+            double x2 = double.Parse(txtX2.Text);
+            double y2 = double.Parse(txtY2.Text);
+            double z2 = double.Parse(txtZ2.Text);
+            vector2 = new Vector3D(x2, y2, z2);
+
+            double angle = vector1.AngleWith(vector2);
+            lblResult.Text = "Angle between Vectors: " + angle.ToString() + " radians";
+
+           
+            pictureBox.Refresh();
+        }
+    }
+    public class Vector3D
+    {
+        private double x;
+        private double y;
+        private double z;
+
+        public Vector3D(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public double Z
+        {
+            get { return z; }
+            set { z = value; }
+        }
+
+        public static Vector3D operator +(Vector3D v1, Vector3D v2)
+        {
+            return new Vector3D(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
+
+        public static Vector3D operator -(Vector3D v1, Vector3D v2)
+        {
+            return new Vector3D(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+        }
+
+        public static double operator *(Vector3D v1, Vector3D v2)
+        {
+            return v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z; // Скалярний добуток
+        }
+
+        public double Length()
+        {
+            return Math.Sqrt(X * X + Y * Y + Z * Z); // Довжина вектора
+        }
+
+        public double AngleWith(Vector3D other)
+        {
+            double dotProduct = this * other; // Скалярний добуток
+            double lengthProduct = this.Length() * other.Length(); // Добуток довжин векторів
+
+            if (lengthProduct == 0)
+                throw new DivideByZeroException("One or both vectors have zero length.");
+
+            return Math.Acos(dotProduct / lengthProduct); // Кут між векторами (в радіанах)
+        }
+    }
+}
